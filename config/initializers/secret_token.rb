bb3bbdd5ed514dfffa2967ae4c4b76d5e65d4851
Michael Rochette <mrochette@macadamian.com>
2013-05-09 10:31:20 -0400

    Heroku init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RochettePortfolio::Application.config.secret_token = '8e5d1db3aaaea9591b5c14125fcca6e42d48d0804b99432964ab50f31ae6d10a6d41ef7c3b64a8eb798369b370d59830bc5a973b73a3d9c12752d469826a7bfa'
